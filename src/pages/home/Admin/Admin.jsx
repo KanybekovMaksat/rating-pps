b6f96d4e805d878af392492e98eb03e2cc889086
Ilya <ilyazhukov128@gmail.com>
2024-06-14 10:43:26 +0600

    Добавлена кнопка регистрации для админа

--- src/pages/home/Admin/Admin.jsx
+++ src/pages/home/Admin/Admin.jsx
@@ -12,7 +12,7 @@ function Admin() {
         <div className="admin__links">
           <Link to='/admin_list' className="admin__link">Список препподователей</Link>
           <Link to='/redact_progres' className="admin__link">Redact Stage</Link>
-          <Link to='/' className="admin__link">Просто кнопка</Link>
+          <Link to='/Registration' className="admin__link">Регистрация</Link>
           <Link to='/' className="admin__link">Просто кнопка</Link>
         </div>
       </div>
