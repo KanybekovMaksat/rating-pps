57dc960762fd424966cfef151da6154940e8698b
Ilya <ilyazhukov128@gmail.com>
2024-06-19 23:00:36 +0600

    Добавленны для PUT запросов в AwardsInfo. Исправлен выбор в AwardsInfoA

--- src/pages/home/Admin/UserInfoA.jsx
+++ src/pages/home/Admin/UserInfoA.jsx
@@ -5,7 +5,7 @@ import NavBar from "../../../components/NavBar";
 
 function UserInfoA() {
   const { id } = useParams();
-  let token = localStorage.getItem('token');
+  const token = localStorage.getItem('token');
   const [userData, setUserData] = useState({});
   const [selectedItems, setSelectedItems] = useState([]);
   const [selectedStages, setSelectedStages] = useState({});
@@ -13,24 +13,22 @@ function UserInfoA() {
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const resp = await axios.get(`https://api.pps.makalabox.com/api/user/account/${id}`, {
+        const response = await axios.get(`https://api.pps.makalabox.com/api/user/account/${id}`, {
           headers: {
             Authorization: `Bearer ${token}`,
           }
         });
-        const data = resp.data;
-        setUserData(data);
+        setUserData(response.data);
       } catch (error) {
-        console.log(error);
+        console.error(error);
       }
     };
 
     fetchUserData();
-  }, [id]);
+  }, [id, token]);
 
   const toggleItemSelection = (itemId, stage) => {
-    const isSelected = selectedItems.includes(itemId);
-    if (isSelected) {
+    if (selectedItems.includes(itemId)) {
       setSelectedItems(selectedItems.filter(item => item !== itemId));
       const newSelectedStages = { ...selectedStages };
       delete newSelectedStages[itemId];
@@ -41,35 +39,59 @@ function UserInfoA() {
     }
   };
 
-  const handleFreezeSelected = async () => {
+  const handleActionSelected = async (action) => {
     try {
       const idBag = selectedItems.map(itemId => ({ id: itemId }));
       const requestData = { idBag };
-      await axios.put(`https://api.pps.makalabox.com/api/admin/freeze`, requestData, {
+      await axios.put(`https://api.pps.makalabox.com/api/admin/${action}`, requestData, {
         headers: {
           Authorization: `Bearer ${token}`
         }
       });
-      location.reload();
+      window.location.reload();
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
 
-  const handleActiveSelected = async () => {
-    try {
-      const idBag = selectedItems.map(itemId => ({ id: itemId }));
-      const requestData = { idBag };
-      await axios.put(`https://api.pps.makalabox.com/api/admin/active`, requestData, {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-      location.reload();
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const handleFreezeSelected = () => handleActionSelected('freeze');
+  const handleActiveSelected = () => handleActionSelected('active');
+
+  const renderUserInfo = (userInfo) => (
+    <div className="userInfo">
+      <div className="userInfo__right">
+        <p className="userInfo__name">ФИО: {userInfo?.name}</p>
+        <p className="userInfo__text">{userInfo?.institut}</p>
+        <p className="userInfo__text">{userInfo?.regular}</p>
+      </div>
+      <div className="userInfo__left">
+        <p className="userInfo__text">{userInfo?.position}</p>
+        <p className="userInfo__text">{userInfo?.email}</p>
+      </div>
+    </div>
+  );
+
+  const renderUserActivity = (title, data) => (
+    data?.length > 0 && (
+      <div className="userSection bline">
+        <h2 className="userInfo__title">{title}</h2>
+        {data.map((item, i) => (
+          <div className="userInfo-in userInfo__text-S" key={item.id} style={{ backgroundColor: i % 2 === 0 ? '#0047FF4D' : '#33FF001A' }}>
+            <p className={`userInfo-in-text ${item.status === 'freeze' ? 'crossed-out' : ''}`}>{item.name}</p>
+            <div>
+              <Link to={item.link}>Link</Link>
+              <input
+                className="check"
+                type="checkbox"
+                checked={selectedItems.includes(item.id)}
+                onChange={() => toggleItemSelection(item.id, item.stage)}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    )
+  );
 
   return (
     <div className="сontents">
@@ -78,97 +100,11 @@ function UserInfoA() {
           <NavBar />
         </div>
         <div className="userData">
-          <div className="userInfo">
-            <div className="userInfo__right">
-              <p className="userInfo__name">ФИО: {userData.userInfo && userData.userInfo.name}</p>
-              <p className="userInfo__text">{userData.userInfo && userData.userInfo.institut}</p>
-              <p className="userInfo__text">{userData.userInfo && userData.userInfo.regular}</p>
-            </div>
-            <div className="userInfo__left">
-              <p className="userInfo__text">{userData.userInfo && userData.userInfo.position}</p>
-              <p className="userInfo__text">{userData.userInfo && userData.userInfo.email}</p>
-            </div>
-          </div>
-          <div className="userAwards bline">
-            {userData.userAwards && userData.userAwards.length > 0 && (
-              <h2 className="userInfo__title">Личные достижения:</h2>
-            )}
-            {userData.userAwards &&
-              userData.userAwards.map((award, i) => (
-                <div className="userInfo-in userInfo__text-S" key={award.id} style={{ backgroundColor: i % 2 === 0 ? '#0047FF4D' : '#33FF001A' }}>
-                  <p className={`userInfo-in-text ${award.status === 'freeze' ? 'crossed-out' : ''}`}>{award.name}</p>
-                  <div>
-                    <Link to={award.link}>Link</Link>
-                    <input
-                      className="check"
-                      type="checkbox"
-                      checked={selectedItems.includes(award.id)}
-                      onChange={() => toggleItemSelection(award.id, award.stage)}
-                    />
-                  </div>
-                </div>
-              ))}
-          </div>
-          <div className="userResearch bline">
-            {userData.userResearch && userData.userResearch.length > 0 && (
-              <h2 className="userInfo__title">Научно-исследовательская деятельность:</h2>
-            )}
-            {userData.userResearch &&
-              userData.userResearch.map((research, i) => (
-                <div className="userInfo-in userInfo__text-S" key={research.id} style={{ backgroundColor: i % 2 === 0 ? '#0047FF4D' : '#33FF001A' }}>
-                  <p className={`userInfo-in-text ${research.status === 'freeze' ? 'crossed-out' : ''}`}>{research.name}</p>
-                  <div>
-                    <Link to={research.link}>Link</Link>
-                    <input
-                      className="check"
-                      type="checkbox"
-                      checked={selectedItems.includes(research.id)}
-                      onChange={() => toggleItemSelection(research.id, research.stage)}
-                    />
-                  </div>
-                </div>
-              ))}
-          </div>
-          <div className="userInnovative bline">
-            {userData.userInnovative && userData.userInnovative.length > 0 && (
-              <h2 className="userInfo__title">Инновационно-образовательная деятельность:</h2>
-            )}
-            {userData.userInnovative &&
-              userData.userInnovative.map((innovative, i) => (
-                <div className="userInfo-in userInfo__text-S" key={innovative.id} style={{ backgroundColor: i % 2 === 0 ? '#0047FF4D' : '#33FF001A' }}>
-                  <p className={`userInfo-in-text ${innovative.status === 'freeze' ? 'crossed-out' : ''}`}>{innovative.name}</p>
-                  <div>
-                    <Link to={innovative.link}>Link</Link>
-                    <input
-                      className="check"
-                      type="checkbox"
-                      checked={selectedItems.includes(innovative.id)}
-                      onChange={() => toggleItemSelection(innovative.id, innovative.stage)}
-                    />
-                  </div>
-                </div>
-              ))}
-          </div>
-          <div className="userSocial bline">
-            {userData.userSocial && userData.userSocial.length > 0 && (
-              <h2 className="userInfo__title">Воспитательная, общественная деятельность:</h2>
-            )}
-            {userData.userSocial &&
-              userData.userSocial.map((social, i) => (
-                <div className="userInfo-in userInfo__text-S" key={social.id} style={{ backgroundColor: i % 2 === 0 ? '#0047FF4D' : '#33FF001A' }}>
-                  <p className={`userInfo-in-text ${social.status === 'freeze' ? 'crossed-out' : ''}`}>{social.name}</p>
-                  <div>
-                    <Link to={social.link}>Link</Link>
-                    <input
-                      className="check"
-                      type="checkbox"
-                      checked={selectedItems.includes(social.id)}
-                      onChange={() => toggleItemSelection(social.id, social.stage)}
-                    />
-                  </div>
-                </div>
-              ))}
-          </div>
+          {renderUserInfo(userData.userInfo)}
+          {renderUserActivity("Личные достижения:", userData.userAwards)}
+          {renderUserActivity("Научно-исследовательская деятельность:", userData.userResearch)}
+          {renderUserActivity("Инновационно-образовательная деятельность:", userData.userInnovative)}
+          {renderUserActivity("Воспитательная, общественная деятельность:", userData.userSocial)}
           <div className="auth__btn-center jc-sb">
             <button className="bnt__log" onClick={handleFreezeSelected}>Заморозить</button>
             <button className="bnt__log" onClick={handleActiveSelected}>Разморозить</button>
