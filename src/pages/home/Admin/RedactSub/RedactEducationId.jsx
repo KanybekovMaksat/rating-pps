1269b5fbf0b4b4e1c97cb407b7d1e8c38d4d8651
quoazh <zhukovilia52@gmail.com>
2024-11-18 21:30:52 +0600

    ???

--- src/pages/home/Admin/RedactSub/RedactEducationId.jsx
+++ src/pages/home/Admin/RedactSub/RedactEducationId.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import NavBar from "../../../../components/NavBar";
 import axios from "axios";
 import { useParams } from "react-router-dom";
